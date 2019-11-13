ca1edafede1d30bc83f9c9132196f42b468e553d
Mai Tien Hai <maitienhai@velacorp.vn>
2019-11-13 23:06:01 +0700

    Updates

---
+++ precache-manifest.36a7cafe1f25d227bbd2198b82357fdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe08b08d1eb5e3bbc27d702ff72a8fda",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bf50dcfe63ad4110b934",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1ff4bbf2db99699fd0cf",
+    "url": "/portfolio/static/js/2.6434b52f.chunk.js"
+  },
+  {
+    "revision": "bf50dcfe63ad4110b934",
+    "url": "/portfolio/static/js/main.a3e91d90.chunk.js"
+  },
+  {
+    "revision": "018405942476b2bee018",
+    "url": "/portfolio/static/js/runtime-main.809edabb.js"
+  }
+]);
